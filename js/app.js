4185c3b8fcf44cf0ebd7f6ec790bf41a8ec188a7
SilvaLu <spvv2706@gmail.com>
2017-11-20 03:17:05 -0500

    añadiendo html

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@ window.onload = inicio;
 // window.addEventListener('load', inicio);
 
 function inicio (){
-document.getElementById(crearText).addEventListener()   
-}
+	var tweetArea = document.getElementById('tweet-area');
+	var tweetBtn = document.getElementById('tweet-btn');
+	var messages = document.getElementById('messages');
+	var count = document.getElementById('count');
+	var MAXCHARACTERS = 140;
